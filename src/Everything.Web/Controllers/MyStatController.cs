e0a26d674defc586088990bcb5ded57f876fa6c3
Pontus Ivarsson <pontus_ivarsson@hotmail.com>
2016-02-09 16:18:14 +0100

    added chart.js with bower, am i really supposed to push wwwroot?

---
+++ src/Everything.Web/Controllers/MyStatController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+
+namespace Everything.Web.Controllers
+{
+    public class MyStatController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
